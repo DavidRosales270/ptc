d290884e230886181d9c9bde331596bf436a8e1d
David de Jesus Rosales Garcia <David de Jesus Rosales Garcia>
2017-08-10 21:23:43 -0600

    agregando dashboard user

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,1 +1,16 @@
-ksdjnnkjdskjd
+@extends('layouts.home')
+@section('content')
+
+<div style="position: relative;  margin: 0; padding: 0; ">
+	<div style="display: inline-block; vertical-align: middle; width: 100%;  margin: 0; padding: 0; ">
+    		{!! csrf_field() !!}
+		
+	
+    
+		
+		
+	</div>
+	<br />
+	<br />
+</div>
+@stop
