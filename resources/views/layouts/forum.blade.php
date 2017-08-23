7854ca57b54ec4a53e65dcb3081f59135e179013
David Rosales <davidrosales270@gmail.com>
2017-08-22 21:41:53 -0600

    modificar estilo en foro

--- resources/views/layouts/forum.blade.php
+++ resources/views/layouts/forum.blade.php
@@ -17,12 +17,12 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
 
-<body style="background-color: #eFeFeF; " >
+<body>
 <div class="container" style="margin: 0; padding: 0; width: 100%; font-size:11px; ">
     <header class="row" style="margin: 0; padding: 0; width: 100%; background-color: #FFF; padding-top: 12px; padding-bottom: 12px;">
         <div class="row" style="margin: 0; padding: 0; width: 100%;">
             <div class="col-sm-6 col-md-6" style="">
-                <a href="/"><img src="../public/imgs/logo.png" alt="" height="56" width="229"></a>
+                <a href="/"><img src="{{ url('/public/imgs/logo.png') }}" alt="" height="56" width="229"></a>
             </div>
             <div class="col-sm-6 col-md-6" style="text-align: center; padding-top: 20px; padding-right: 20px;  ">
                 @if (!Auth::check() || Auth::user()->name != "admin")
@@ -48,7 +48,8 @@
 <hr style="background-color: red; border: 4px solid red; padding: 0; margin: 0; " />
 
 <div class="row" >
-    <div id="main" style="width: 90%; margin: 20px auto">
+    <div id="main" class="main">
+        <h2>Foro</h2>
         @yield('content')
     </div>
 
